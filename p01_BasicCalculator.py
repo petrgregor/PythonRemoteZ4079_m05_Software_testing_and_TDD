b4a7471b5e37e80c414ef9e7f79dfa64c468ecda
Petr Gregor <petr.gregor@email.cz>
2024-12-16 19:38:42 +0100

    Basic calculator + tests.

---
+++ p01_BasicCalculator.py
@@ -0,0 +1,60 @@
+class BasicCalculator:
+
+    def add(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 (isinstance(num2, float)))):
+            return num1 + num2
+        return None
+
+    def subtract(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 (isinstance(num2, float)))):
+            return num1 - num2
+        return None
+
+    def multiply(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 (isinstance(num2, float)))):
+            return num1 * num2
+        return None
+
+    """def divide(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 (isinstance(num2, float))) and
+                num2 != 0):
+            return num1 / num2
+        return None"""
+
+    def divide(self, num1, num2):
+        if ((isinstance(num1, int) or
+             isinstance(num1, float)) and
+                (isinstance(num2, int) or
+                 (isinstance(num2, float)))):
+            try:
+                return num1 / num2
+            except:
+                return None
+        return None
+
+
+if __name__ == '__main__':
+    basic_calculator = BasicCalculator()
+    num1 = float(input("Zadejte první číslo: "))
+    op = input("Zadejte požadovanou operaci [+, -, *, /]: ")
+    num2 = float(input("Zadejte druhé číslo: "))
+    if op == '+':
+        print(f"{num1} {op} {num2} = {basic_calculator.add(num1, num2)}")
+    if op == '-':
+        print(f"{num1} {op} {num2} = {basic_calculator.subtract(num1, num2)}")
+    if op == '*':
+        print(f"{num1} {op} {num2} = {basic_calculator.multiply(num1, num2)}")
+    if op == '/':
+        print(f"{num1} {op} {num2} = {basic_calculator.divide(num1, num2)}")
---
+++ p01_BasicCalculator_test.py
@@ -0,0 +1,65 @@
+import pytest
+from p01_BasicCalculator import BasicCalculator
+
+
+def test_addition():
+    basic_calculator = BasicCalculator()
+    result = basic_calculator.add(3, 5)
+    assert result == 8
+    assert basic_calculator.add(-6, -4) == -10
+    assert basic_calculator.add(-3, 4) == 1
+    assert basic_calculator.add(3, -4) == -1
+    assert basic_calculator.add(3, 0) == 3
+    assert basic_calculator.add("3", "5") is None
+    assert basic_calculator.add("Hello", "World") is None
+    assert basic_calculator.add(3, "5") is None
+    assert round(basic_calculator.add(2.1, 3.2), 1) == 5.3
+    assert basic_calculator.add([5], 2) is None
+
+
+def test_subtraction():
+    basic_calculator = BasicCalculator()
+    result = basic_calculator.subtract(3, 5)
+    assert result == -2
+    assert basic_calculator.subtract(-6, -4) == -2
+    assert basic_calculator.subtract(-3, 4) == -7
+    assert basic_calculator.subtract(3, -4) == 7
+    assert basic_calculator.subtract(3, 0) == 3
+    assert basic_calculator.subtract(0, 3) == -3
+    assert basic_calculator.subtract("3", "5") is None
+    assert basic_calculator.subtract("Hello", "World") is None
+    assert basic_calculator.subtract(3, "5") is None
+    assert round(basic_calculator.subtract(2.1, 3.2), 1) == -1.1
+    assert basic_calculator.subtract([5], 2) is None
+
+
+def test_multiply():
+    basic_calculator = BasicCalculator()
+    result = basic_calculator.multiply(3, 5)
+    assert result == 15
+    assert basic_calculator.multiply(-6, -4) == 24
+    assert basic_calculator.multiply(-3, 4) == -12
+    assert basic_calculator.multiply(3, -4) == -12
+    assert basic_calculator.multiply(3, 0) == 0
+    assert basic_calculator.multiply(0, 3) == 0
+    assert basic_calculator.multiply("3", "5") is None
+    assert basic_calculator.multiply("Hello", "World") is None
+    assert basic_calculator.multiply(3, "5") is None
+    assert round(basic_calculator.multiply(2.1, 3.2), 2) == 6.72
+    assert basic_calculator.multiply([5], 2) is None
+
+
+def test_divide():
+    basic_calculator = BasicCalculator()
+    result = basic_calculator.divide(8, 2)
+    assert result == 4
+    assert basic_calculator.divide(-12, -4) == 3
+    assert basic_calculator.divide(-12, 4) == -3
+    assert basic_calculator.divide(12, -4) == -3
+    assert basic_calculator.divide(3, 0) is None
+    assert basic_calculator.divide(0, 3) == 0
+    assert basic_calculator.divide("3", "5") is None
+    assert basic_calculator.divide("Hello", "World") is None
+    assert basic_calculator.divide(3, "5") is None
+    assert round(basic_calculator.divide(2.1, 3.2), 5) == 0.65625
+    assert basic_calculator.divide([5], 2) is None

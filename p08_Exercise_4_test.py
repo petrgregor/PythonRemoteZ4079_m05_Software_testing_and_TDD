c780520dc1ffb957a4f243926fe364899453ff27
Petr Gregor <petr.gregor@email.cz>
2024-12-17 23:29:55 +0100

    Exercise 4 - parametrized test.

--- p08_Exercise_4_test.py
+++ p08_Exercise_4_test.py
@@ -3,6 +3,9 @@ Funkce get_avg bere ze souboru how_much čísla a počítá jejich průměr. Ote
 že očekává pouze čísla v souboru a nemůžeš použít soubor k testování.
 """
 from unittest.mock import MagicMock, patch
+
+import pytest
+
 from p08_Exercise_4 import *
 
 
@@ -18,3 +21,11 @@ def test_get_avg():
     fake_get_data = MagicMock(return_value="123")
     with patch("p08_Exercise_4._get_data", fake_get_data):
         assert get_avg(3) == 2
+
+
+@pytest.mark.parametrize("how_much, result", [(1, 1), (2, 1.5), (3, 2), (4, 2.5), (5, 3),
+                                              (6, 3.5), (7, 4), (8, 4.5), (9, 5)])
+def test_func(how_much, result):
+    fake_get_data = MagicMock(return_value=''.join(str(i) for i in range(1, how_much+1)))
+    with patch("p08_Exercise_4._get_data", fake_get_data):
+        assert get_avg(how_much) == result
